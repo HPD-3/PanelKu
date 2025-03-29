13f54e5b145e2a152003966ea3aa42f36f43e778
HPD-3 <hapidh333@gmail.com>
2025-03-30 05:49:44 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import './styles.css';
 import { loginScripts  } from './login.js';
 import { loadDashboard } from './dashboard.js';
 import { responsive } from './responsive.js'
+import { updateAccountName,updateBio } from "./customer.js"; 
+
+updateAccountName(); 
 loadDashboard();
 loginScripts();
 responsive();
+updateBio();
 
